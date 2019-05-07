1c1b68cc082724b51677485cdc8a2f9b31af79b1
louyilin <louyilincu@163.com>
2019-05-07 14:46:44 +0800

    c除了time都更新完了

--- douban_spider_keshe/data_analyze/change_CSV/changetime.py
+++ douban_spider_keshe/data_analyze/change_CSV/changetime.py
@@ -7,4 +7,6 @@ import pymysql
 
 df=pd.read_csv(r'D:\pythonPractise\douban_spider_keshe\data_analyze\change_CSV\change_type2.csv',encoding='utf-8',names=['编号','名字','导演','编剧','主演一','主演二','主演三','类型','时长','年份','地区','语言','分数'])
 pd.set_option('display.max_rows',None)    #把数据全部行展示出来
-df_country=df['地区'].tolist()
+df_year=df['年份'].tolist()
+print(df_year)
+# for year in df_year:
